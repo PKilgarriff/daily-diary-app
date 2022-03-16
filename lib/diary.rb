5b0ffcb68b0783b93b5d8ebf40fe9922894c2f1a
Paul <pkilgarriff@gmail.com>
2022-03-16 21:08:04 +0000

    Update Diary class
    - change hardcoded diary entries to database queries
    - add database_connection method
    - add database_query method
      - both are good candidates for extraction
    - add explicit initialize with PG injection

--- lib/diary.rb
+++ lib/diary.rb
@@ -1,9 +1,23 @@
+require 'pg'
+
 class Diary
+  def initialize(postgres = PG, database)
+    database_connection(postgres, database)
+  end
+
   def list_all_entries
-    [
-      "Tuesday: Hello World!",
-      "Wednesday: Podcasts I Like",
-      "Thursday: St Patrick\\'s Day"
-    ].join("\n")
+    output = []
+    database_query.each do |row|
+      output << "#{row['title']}: #{row['url']}"
+    end
+    return output
+  end
+
+  def database_connection(postgres = PG, database)
+    @connection = postgres.connect(dbname: database)
+  end
+
+  def database_query
+    @connection.exec "SELECT * FROM entries"
   end
 end
--- spec/units/diary_spec.rb
+++ spec/units/diary_spec.rb
@@ -1,11 +1,31 @@
 require 'diary'
 
 describe Diary do
-  subject(:diary) { described_class.new }
+  let(:fake_diary_entry1) { { 'title' => "Tuesday", 'url' => "Hello World!" } }
+  let(:fake_diary_entry2) { { 'title' => "Wednesday", 'url' => "Podcasts I Like" } }
+  let(:fake_diary_entry3) { { 'title' => "Thursday", 'url' => "St Patrick's Day" } }
+  let(:fake_entries) { [ fake_diary_entry1, fake_diary_entry2, fake_diary_entry3 ] }
+  let(:fake_connection) { double(:connection_double, :exec => fake_entries) }
+  let(:fake_pg) { double(:pg_double, :connect => fake_connection) }
+  subject(:diary) { described_class.new(fake_pg, 'daily_diary_test') }
 
   describe '#list_all_entries' do
     it 'returns a list of entries' do
-      expect(diary.list_all_entries).to eq ("Tuesday: Hello World!\nWednesday: Podcasts I Like\nThursday: St Patrick\\'s Day")
+      expected_entries = ["Tuesday: Hello World!", "Wednesday: Podcasts I Like", "Thursday: St Patrick's Day"]
+      expect(diary.list_all_entries).to match_array(expected_entries)
+    end
+  end
+
+  describe '#database_connection' do
+    it 'connects to daily_dairy database' do
+      expect(diary.database_connection(fake_pg, 'daily_diary_test')).to eq fake_connection
+    end
+  end
+
+  describe '#database_query' do
+    it 'sends a query to the daily_dairy database' do
+      diary.database_connection(fake_pg, 'daily_diary_test')
+      expect(diary.database_query).to eq fake_connection.exec
     end
   end
 end
